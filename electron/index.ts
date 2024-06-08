741a261e9912e2535f45e5aa6defc5452bc790a8
Paolo <pauloportes.guimalan187@gmail.com>
2024-06-08 21:21:02 +0800

    feat: requests and interfaces has been declared

--- electron/index.ts
+++ electron/index.ts
@@ -38,7 +38,7 @@ function createWindow() {
   // and load the index.html of the app.
   if (isDev) {
     window?.loadURL(url);
-    window.webContents.openDevTools();
+    // window.webContents.openDevTools();
     setTimeout(() => {
       window.reload();
       window.webContents
