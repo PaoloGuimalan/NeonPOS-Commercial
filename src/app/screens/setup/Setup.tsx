781ad786a4b5a9ed2dd1910969eae2c0e2d058ea
Magszz <benitua.magno@gmail.com>
2024-06-19 15:03:46 +0800

    feat: use bglayout

--- src/app/screens/setup/Setup.tsx
+++ src/app/screens/setup/Setup.tsx
@@ -1,27 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import NeonPOSSVG from '../../../assets/NeonPOS_BG.svg'
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Startup from '../../reusables/Startup';
 import Formtab from './tabs/Formtab';
+import BGLayout from '@/app/reusables/BGLayout';
 
 function Setup() {
-
   const [isLoaded, setisLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     setTimeout(() => {
-        setisLoaded(true);
-    },10000);
-  },[]);
+      setisLoaded(true);
+    }, 10000);
+  }, []);
 
   return (
-    <div style={{ background: `url(${NeonPOSSVG})`, backgroundSize: "cover", backgroundPosition: "bottom", backgroundRepeat: "no-repeat" }} className='w-full h-full absolute flex items-center justify-center bg-transparent'>
-        <Routes>
-            <Route path='/' element={isLoaded ? <Navigate to="/setup/form" /> : <Startup />} />
-            <Route path='/form' element={isLoaded ? <Formtab /> : <Navigate to="/setup" />} />
-        </Routes>
-    </div>
-  )
+    <BGLayout className="absolute flex items-center justify-center bg-transparent">
+      <Routes>
+        <Route path="/" element={isLoaded ? <Navigate to="/setup/form" /> : <Startup />} />
+        <Route path="/form" element={isLoaded ? <Formtab /> : <Navigate to="/setup" />} />
+      </Routes>
+    </BGLayout>
+  );
 }
 
 export default Setup;
